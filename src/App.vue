35783315d0d310f9b932ec9ab43e9b3187daf401
wangjingjing2 <wangjingjing2@163.com>
2020-08-06 13:31:36 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,36 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-button>el-button</el-button>
+    <!-- 如果:label,1就为数值，如果是label，1就是字符串 -->
+    <!-- <el-radio v-model="radio" :label="1">选项1</el-radio>
+    <el-radio v-model="radio" :label="2">选项2</el-radio>-->
+    <!-- <el-radio-group v-model="radio" size="small">
+      <el-radio :label="3" @change="change" border>3</el-radio>
+      <el-radio :label="6" @change="change" border>6</el-radio>
+      <el-radio :label="9" @change="change" border>9</el-radio>
+    </el-radio-group> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app",
+  data() {
+    return {
+			// radio: 6,
+			
+    };
+  },
+	components: {},
+	methods: {
+		// change (e) {
+		// 	console.log(e);// 3 6 9
+		// }
 
 
+	}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
