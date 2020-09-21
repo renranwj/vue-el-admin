29329500ac7495c06e2f571f854e092b2e58f7dc
wangjingjing <renranwj@163.com>
2020-09-21 22:16:13 +0800

    add

--- src/store/modules/goods_create.js
+++ src/store/modules/goods_create.js
@@ -1,38 +1,39 @@
 import $util from '@/common/util'
 export default {
     state: {
-        spec_type: 0,//商品规格：0 单一规格；1 多规格
+        spec_type: 0, //商品规格：0 单一规格；1 多规格
         title: "",
         category: [], //种类
         desc: "", //描述
-        unit: "",//单位
-        stock: 0,//总库存
+        unit: "", //单位
+        stock: 0, //总库存
         min_stock: 0, //库存预警
-        display_stock: 0,//库存显示
-        status: 0,//是否上架 0 仓库  ；1 上架
-        express: "",//运费模板
+        display_stock: 0, //库存显示
+        status: 0, //是否上架 0 仓库  ；1 上架
+        express: "", //运费模板
 
-        oprice: 0,//市场价格
-        pprice: 0,//销售价格
-        cprice: 0,//成本价格
-        weight: 0,//重量
-        volume: 0,//体积
+        oprice: 0, //市场价格
+        pprice: 0, //销售价格
+        cprice: 0, //成本价格
+        weight: 0, //重量
+        volume: 0, //体积
 
         // 规格卡片
-        spec_card: [
-            {
-                name: '颜色',
-                type: 0, // 0 无，1 颜色，2 图片
-                list: []
-            }
-        ]
+        spec_card: [{
+            name: '颜色',
+            type: 0, // 0 无，1 颜色，2 图片
+            list: []
+        }]
     },
     getters: {
 
     },
     mutations: {
         //修改state
-        vModelState(state, { key, value }) {
+        vModelState(state, {
+            key,
+            value
+        }) {
             state[key] = value;
         },
 
@@ -50,12 +51,19 @@ export default {
             state.spec_card.splice(index, 1);
         },
         // 修改规格卡片
-        vModelCard (state, {key, index, value}) {
+        vModelCard(state, {
+            key,
+            index,
+            value
+        }) {
             state.spec_card[index][key] = value;
         },
 
         //规格卡片排序
-        sortSpecCard (state, {action, index}) {
+        sortSpecCard(state, {
+            action,
+            index
+        }) {
             $util[action](state.spec_card, index);
         }
 
